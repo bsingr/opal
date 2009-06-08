f07112284985c83158c95bcd5991b82e24617397
Adam Beynon <adambeynon@adam-beynons-macbook-air.local>
2009-06-08 17:15:53 +0100

    All

---
+++ apps/Carino/main.m
@@ -0,0 +1,14 @@
+// 
+//  main.m
+//  Carino
+//  
+//  Created by Adam Beynon on 2009-06-08.
+//  Copyright 2009 Adam Beynon. All rights reserved.
+// 
+
+#import <Vienna/Vienna.h>
+
+int main(int argc, char *argv[])
+{	
+    return NSApplicationMain();
+}
