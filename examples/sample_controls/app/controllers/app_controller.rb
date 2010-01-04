63b83db63d6aa011957fa1c27fac7d1d953615f6
Adam Beynon <adam@adambeynon.com>
2010-01-04 10:52:09 +0000

    moved things about

---
+++ examples/sample_controls/app/controllers/app_controller.rb
@@ -0,0 +1,11 @@
+# Delegate of VN::Application
+class AppController
+  
+  def app_will_finish_launching(notification)
+    # do stuff before app finishes
+  end
+  
+  def app_did_finish_launching(notification)
+    # do stuff after app finishes
+  end
+end
